60dd6773026791ba3b8587c1c1aa5cc55df7590f
patuaans <phananhtuan849@gmail.com>
2024-12-28 07:41:22 +0700

    delete unuse code

--- train.py
+++ train.py
@@ -21,7 +21,6 @@ def get_quantization_config():
         bnb_4bit_quant_type="nf4",
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=torch.bfloat16,
-        # llm_int8_enable_fp32_cpu_offload=True,
     )
 
 def train_model(model, tokenizer, tokenized_datasets, result_path):
